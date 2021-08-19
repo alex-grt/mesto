bfbbfb9a4b5f4c93615600c578b444f191a43d56
Александр Карякин <dr.gr00ter@yandex.ru>
2021-08-19 13:18:34 +0300

    Прооектная работа <Место> (спринт 4)
    - создана страница сайта согласно макету
    - написан скрипт JS, добавляющий интерактивность странице

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+/* общие переменные */
+
+let popup = document.querySelector('.popup');
+let formEdit = document.querySelector('.form-edit');
+let inputName = formEdit.querySelector('.form-edit__name');
+let inputDescription = formEdit.querySelector('.form-edit__description');
+let profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile-card__title');
+let profileDescription = profile.querySelector('.profile-card__text');
+
+/* секция открытия попапа с формой редактирования профиля */
+
+let buttonEdit = document.querySelector('.button-edit');
+
+function formEditOpen() {
+  popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+}
+
+buttonEdit.addEventListener('click', formEditOpen);
+
+/* секция закрытия попапа с формой редактирования профиля */
+
+let buttonClose = document.querySelector('.button-close');
+
+function formEditClose() {
+  popup.classList.remove('popup_opened');
+}
+
+buttonClose.addEventListener('click', formEditClose);
+
+/* секция отправки данных, введённых в форму редактирования профиля */
+
+let buttonSubmit = formEdit.querySelector('.button-submit');
+
+function formEditSubmit (evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  popup.classList.remove('popup_opened');
+}
+
+formEdit.addEventListener('submit', formEditSubmit);
