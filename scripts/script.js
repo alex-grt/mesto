17e57444df71778efbd2f8467c7dd93a58d0027f
Александр Карякин <dr.gr00ter@yandex.ru>
2021-08-21 19:42:01 +0300

    HOTFIXES
    CSS
    - блоку popup добавлен брейкпоинт 320 со свойством width в 100vw
    - блоку profile добавлен брейкпоинт 320 со свойством width и вычисляемым значением
    - блоку footer добавлен брейкпоинт 320 со свойством width и вычисляемым значением
    
    Надо исправить
    HTML
    - для блока popup тег section заменён на div
    - форма редактирования профиля помещена в дополнительную обёртку
    - форме редактирования профиля присвоен атрибут name
    - полям ввода присвоен общий класс и добавлены модификаторы
    
    CSS
    - добавлен отступ у логотипа на мобильных разрешениях
    - в блоке profile удалено ограничение высоты на мобильных разрешениях
    
    JavaScript
    - в функцию отправки данных добавлено обращение к функции закрытия формы
    
    Можно лучше
    HTML
    - кнопкам добавлен атрибут aria-label
    - заголовок формы редактирования профиля вынесен из формы
    - секция popup перенесена в конец страницы
    - убраны лишние обёртки и заменены соответствующие теги в блоке place
    
    CSS
    - у полей ввода удалён outline
    
    JavaScript
    - переименованы все функции
    - отслеживания действий пользователя перенесены в конец скрипта
    - объявление всех переменных выполнено в начале скрипта

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,41 @@
-/* общие переменные */
+/* объявление переменных */
 
 let popup = document.querySelector('.popup');
 let formEdit = document.querySelector('.form-edit');
-let inputName = formEdit.querySelector('.form-edit__name');
-let inputDescription = formEdit.querySelector('.form-edit__description');
+let inputName = formEdit.querySelector('.form-edit__input_asgmt_name');
+let inputDescription = formEdit.querySelector('.form-edit__input_asgmt_description');
+let buttonClose = formEdit.querySelector('.form-edit__button-close');
+let buttonSubmit = formEdit.querySelector('.form-edit__button-submit');
 let profile = document.querySelector('.profile');
 let profileName = profile.querySelector('.profile-card__title');
 let profileDescription = profile.querySelector('.profile-card__text');
+let buttonEdit = profile.querySelector('.profile-card__button-edit');
 
 /* секция открытия попапа с формой редактирования профиля */
 
-let buttonEdit = profile.querySelector('.profile-card__button-edit');
-
-function formEditOpen() {
+function openFormEdit() {
   popup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
 }
 
-buttonEdit.addEventListener('click', formEditOpen);
-
 /* секция закрытия попапа с формой редактирования профиля */
 
-let buttonClose = formEdit.querySelector('.form-edit__button-close');
-
-function formEditClose() {
+function closeFormEdit() {
   popup.classList.remove('popup_opened');
 }
-
-buttonClose.addEventListener('click', formEditClose);
 
 /* секция отправки данных, введённых в форму редактирования профиля */
 
-let buttonSubmit = formEdit.querySelector('.form-edit__button-submit');
-
-function formEditSubmit (evt) {
+function submitFormEdit (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
-  popup.classList.remove('popup_opened');
+  closeFormEdit();
 }
 
-formEdit.addEventListener('submit', formEditSubmit);
+/* секция отслеживания действий пользователя */
+
+buttonEdit.addEventListener('click', openFormEdit);
+buttonClose.addEventListener('click', closeFormEdit);
+formEdit.addEventListener('submit', submitFormEdit);
